e27c1ca771f9c87e1ab8a6b8f065ec19c80af7aa
Jon Burgess <jkburges@gmail.com>
2015-02-23 16:23:43 +1100

    Cookbook and recipe which runs database migrations (liquibase in the current implementation).
    It's intended use in the near term is for the marvl data atlas but hopefully it can be useful (generic enough) for other uses.

---
+++ cookbooks/database_migrations/attributes/default.rb
@@ -0,0 +1,4 @@
+default['database_migrations']['base_dir'] = '/var/lib/database_migrations'
+default['database_migrations']['user'] = 'migrations'
+default['database_migrations']['group'] = 'migrations'
+default['database_migrations']['migrations'] = []
