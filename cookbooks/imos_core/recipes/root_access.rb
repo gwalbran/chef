83b33b2ead31146faa814b7cf23d7796547bc5f6
Dan Fruehauf <malkodan@gmail.com>
2015-04-21 11:36:29 +1000

    imos_core: root password and keys from data bag

--- cookbooks/imos_core/recipes/root_access.rb
+++ cookbooks/imos_core/recipes/root_access.rb
@@ -1,26 +1,33 @@
 #
-# Cookbook Name:: root_access
-# Recipe:: lvm
+# Cookbook Name:: imos_core
+# Recipe:: root_access
 #
 # Copyright 2013, IMOS
 #
 # All rights reserved - Do Not Redistribute
 #
 
-
-# Hardcoded keys for root for Dan & Julian
 directory "/root/.ssh" do
   mode 00755
 end
+
+root_data_bag = Chef::EncryptedDataBagItem.load("passwords", "root")
 
 template "/root/.ssh/authorized_keys" do
   source "root_authorized_keys.erb"
   mode   00644
   owner  "root"
   group  "root"
+  variables({
+    :ssh_keys => root_data_bag['ssh_keys']
+  })
 end
 
 # Change root password override EVERYTHING
-execute "IMOS root password" do
-  command "usermod -p '$6$aVWuSWde$hBJg4mtafv7D1EN./HyOQWairw2ty6z/lfkXwpdqfwv5Qqrp1fy6mCknO4XdIDRLyAh2UL.bT.lthW4QYAm4A0' root"
+execute "root password" do
+  command "usermod -p '#{root_data_bag['password']}' root"
+  not_if {
+    current_root_password=`getent shadow root`.split(':')[1]
+    current_root_password == root_data_bag['password']
+  }
 end
