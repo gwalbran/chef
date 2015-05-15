f1bf4cdb067275397af45d4a51f9d7d4792f55c7
Dan Fruehauf <malkodan@gmail.com>
2015-05-15 17:55:23 +1000

    bin/prepare-box.sh: indent with spaces instead of tabs

--- bin/prepare-box.sh
+++ bin/prepare-box.sh
@@ -15,12 +15,12 @@ export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 # remove ssh host identities, they should be recreated on reboot
 remove_ssh_host_identities() {
-	rm -f /etc/ssh/ssh_host_*
+    rm -f /etc/ssh/ssh_host_*
 
-	# ubuntu is rather dumb, so we'll add something in rc.local for it
-	# to recreate the keys on the next restart
-	echo "#!/bin/bash" > /etc/rc.local
-	echo "test -f /etc/ssh/ssh_host_rsa_key || dpkg-reconfigure openssh-server" >> /etc/rc.local
+    # ubuntu is rather dumb, so we'll add something in rc.local for it
+    # to recreate the keys on the next restart
+    echo "#!/bin/bash" > /etc/rc.local
+    echo "test -f /etc/ssh/ssh_host_rsa_key || dpkg-reconfigure openssh-server" >> /etc/rc.local
 }
 
 # clear network rules so box can boot cleanly
@@ -32,41 +32,41 @@ clear_udev_net_rules() {
 
 # remove all users except for the ones in $USERS_IGNORE
 remove_users() {
-	local user
-	for user in `ls -1 /home | grep -v 'lost+found'`; do
-		if ! echo $USERS_IGNORE | grep -q "\b$user\b"; then
-			userdel -r -f $user
-		fi
-	done
-	# userdel might return errors - we don't care!!
-	return 0
+    local user
+    for user in `ls -1 /home | grep -v 'lost+found'`; do
+        if ! echo $USERS_IGNORE | grep -q "\b$user\b"; then
+            userdel -r -f $user
+        fi
+    done
+    # userdel might return errors - we don't care!!
+    return 0
 }
 
 # setup password for root user
 setup_root_password() {
-	echo root:$ROOT_PASSWORD | chpasswd
+    echo root:$ROOT_PASSWORD | chpasswd
 }
 
 # clear history for root
 clear_root_history() {
-	> /root/.bash_history
+    > /root/.bash_history
 }
 
 # clear /tmp/
 clear_tmp() {
-	# might encounter some locked files, as chef-solo will be mounted there
-	# run with --one-file-system so we don't delete our chef stuff
-	rm -rf --one-file-system --preserve-root /tmp/*; true
+    # might encounter some locked files, as chef-solo will be mounted there
+    # run with --one-file-system so we don't delete our chef stuff
+    rm -rf --one-file-system --preserve-root /tmp/*; true
 }
 
 # main
 main() {
-	remove_ssh_host_identities && \
+    remove_ssh_host_identities && \
     clear_udev_net_rules && \
-	remove_users && \
-	setup_root_password && \
-	clear_root_history && \
-	clear_tmp && \
+    remove_users && \
+    setup_root_password && \
+    clear_root_history && \
+    clear_tmp && \
     poweroff
 }
 
