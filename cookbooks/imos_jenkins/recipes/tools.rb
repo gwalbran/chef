db59ce97a3ab0b9dbf67040364ee1987b79b8724
Dan Fruehauf <malkodan@gmail.com>
2015-06-16 10:12:31 +1000

    imos_jenkins: added tools recipe
    as before, provides more readability when configuring master

---
+++ cookbooks/imos_jenkins/recipes/tools.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: jenkins
+# Recipe:: tools
+#
+# Copyright 2015, IMOS
+#
+# All rights reserved - Do Not Redistribute
+#
+# Build configuration required on master
+
+include_recipe "imos_jenkins::maven_installer"
+include_recipe "imos_jenkins::grails_installer"
+include_recipe "imos_jenkins::node_common"
