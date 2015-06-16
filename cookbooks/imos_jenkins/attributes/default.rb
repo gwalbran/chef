775c50ee967531465f48a7dcac497777e0b30460
Dan Fruehauf <malkodan@gmail.com>
2015-06-16 19:09:10 +1000

    imos_jenkins: install ant tooling

--- cookbooks/imos_jenkins/attributes/default.rb
+++ cookbooks/imos_jenkins/attributes/default.rb
@@ -5,6 +5,7 @@ default['imos_jenkins']['master_url'] = "https://jenkins.aodn.org.au/"
 
 default['imos_jenkins']['maven']['version']   = "3.2.2"
 default['imos_jenkins']['grails']['versions'] = [ "1.3.7", "2.1.0", "2.2.0", "2.4.4" ]
+default['imos_jenkins']['ant']['versions']    = [ "1.8.4" ]
 
 # Global environment settings for master
 default['imos_jenkins']['username']  = "jenkins"
