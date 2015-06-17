6d5d2e4a75a6f5d66509519bd0b455c2d57a3f6b
Dan Fruehauf <malkodan@gmail.com>
2015-06-18 09:35:12 +1000

    imos_jenkins: unify ant, grails, maven installers
    same groovy snippet was used for ant, grails and maven installers,
    now factored into one recipe

--- cookbooks/imos_jenkins/recipes/grails_installer.rb
+++ cookbooks/imos_jenkins/recipes/grails_installer.rb
@@ -1,29 +1,20 @@
+#
+# Cookbook Name:: jenkins
+# Recipe:: grails_installer
+#
+# Copyright 2015, IMOS
+#
+# All rights reserved - Do Not Redistribute
+#
+
+grails_installer = Chef::Recipe::JenkinsHelper.groovy_code_for_tool_installer(
+  node['imos_jenkins']['grails']['versions'],
+  "com.g2one.hudson.grails",
+  "GrailsInstallation",
+  "GrailsInstaller",
+  "GrailsInstallation"
+)
+
 jenkins_script 'grails autoinstaller' do
-  command <<-GROOVY
-    import jenkins.model.*
-    import com.g2one.hudson.grails.*
-    import hudson.tools.InstallSourceProperty
-
-    def requiredGrailsVersions = #{node['imos_jenkins']['grails']['versions']}
-
-    def extensions = Jenkins.instance.getExtensionList(com.g2one.hudson.grails.GrailsInstallation.DescriptorImpl.class)[0]
-    def grailsInstallations = (extensions.installations as List)
-
-    def installedGrailsVersions = grailsInstallations.collect { it.getName() }
-
-    def grailsVersionsToInstall = requiredGrailsVersions - installedGrailsVersions
-
-    if (!grailsVersionsToInstall.isEmpty()) {
-      def newGrailsInstallations = grailsInstallations
-      grailsVersionsToInstall.each { grailsVersion ->
-        println "Installing ${grailsVersion}"
-        def grailsAutoInstaller = new GrailsInstaller(grailsVersion)
-        def installProperty = new InstallSourceProperty([grailsAutoInstaller])
-        def autoInstallation = new GrailsInstallation(grailsVersion, "", [installProperty])
-        newGrailsInstallations.add(autoInstallation)
-      }
-      extensions.installations = newGrailsInstallations
-      extensions.save()
-    }
-    GROOVY
+  command grails_installer
 end
