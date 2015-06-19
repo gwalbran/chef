42558d9d2d0f964d6947db68ea8fbfe2a048eed4
Dan Fruehauf <malkodan@gmail.com>
2015-06-19 16:30:18 +1000

    imos_devel: refactor talend recipe
     * more generic way of installing talend packages
     * install mdb driver

--- cookbooks/imos_devel/recipes/talend.rb
+++ cookbooks/imos_devel/recipes/talend.rb
@@ -9,43 +9,40 @@
 
 package 'zip'
 
-talend_pkg_cache = ::File.join(Chef::Config[:file_cache_path], ::File.basename(node['imos_devel']['talend']['source_url']))
-remote_file talend_pkg_cache do
-  source   node['imos_devel']['talend']['source_url']
-  checksum node['imos_devel']['talend']['source_checksum']
-  mode     0644
-  action   :create_if_missing
+def install_talend_pkg(pkg_desc)
+  talend_pkg_basename = ::File.basename(pkg_desc['source_url'])
+  talend_pkg_cache_file = ::File.join(Chef::Config[:file_cache_path], talend_pkg_basename)
+
+  remote_file talend_pkg_cache_file do
+    source   pkg_desc['source_url']
+    checksum pkg_desc['checksum']
+    mode     0644
+    action   :create_if_missing
+    notifies :run, "execute[deploy_talend_pkg_#{talend_pkg_basename}]", :immediately
+  end
+
+  install_to = ::File.join(node['imos_devel']['talend']['install_dir'], pkg_desc['install_to'])
+
+  if pkg_desc['unzip']
+    unzip_filter = pkg_desc['unzip_filter'] or ""
+    Chef::Log.info "Unpacking '#{talend_pkg_cache_file}' -> '#{install_to}' using filter '#{unzip_filter}'"
+    execute "deploy_talend_pkg_#{talend_pkg_basename}" do
+      command "tmp_dir=`mktemp -d` && unzip -q -u -o -d $tmp_dir #{talend_pkg_cache_file} && mkdir -p #{install_to} && cp -a $tmp_dir/#{unzip_filter} #{install_to} && rm -rf --preserve-root $tmp_dir"
+      action :nothing
+    end
+  else
+    Chef::Log.info "Copying '#{talend_pkg_cache_file}' -> '#{install_to}'"
+    puts "cp #{talend_pkg_cache_file} #{install_to}"
+    execute "deploy_talend_pkg_#{talend_pkg_basename}" do
+      command "mkdir -p #{install_to} && cp #{talend_pkg_cache_file} #{install_to}"
+      action :nothing
+    end
+  end
+
 end
 
-talend_extracted_dir = talend_pkg_cache.split('.')[0...-1].join('.')
-execute "unpack_talend" do
-  command "unzip -q -u -o -d #{Chef::Config[:file_cache_path]} #{talend_pkg_cache} && mv #{talend_extracted_dir} #{node['imos_devel']['talend']['install_dir']}"
-  not_if { ::File.exist?(node['imos_devel']['talend']['install_dir']) }
-end
-
-# SDI plugins for talend
-talend_sdi_pkg_cache = ::File.join(Chef::Config[:file_cache_path], ::File.basename(node['imos_devel']['talend']['sdi_source_url']))
-remote_file talend_sdi_pkg_cache do
-  source   node['imos_devel']['talend']['sdi_source_url']
-  checksum node['imos_devel']['talend']['sdi_source_checksum']
-  mode     0644
-  action   :create_if_missing
-end
-
-talend_sdi_extracted_dir = talend_sdi_pkg_cache.split('.')[0...-1].join('.')
-talend_sdi_plugins_dir = ::File.join(node['imos_devel']['talend']['install_dir'], "plugins", "org.talend.libraries.sdi_4.2.0")
-execute "unpack_talend_sdi" do
-  command "unzip -q -u -o -d #{Chef::Config[:file_cache_path]} #{talend_sdi_pkg_cache} && mv #{talend_sdi_extracted_dir}/plugins/* #{node['imos_devel']['talend']['install_dir']}/plugins/ && rm -rf #{talend_sdi_extracted_dir}"
-  not_if { ::File.exist?(talend_sdi_plugins_dir) }
-end
-
-# Codegen plugin for talend
-talend_codegen_basename = ::File.basename(node['imos_devel']['talend']['codegen_source_url'])
-remote_file ::File.join(node['imos_devel']['talend']['install_dir'], "plugins", talend_codegen_basename) do
-  source   node['imos_devel']['talend']['codegen_source_url']
-  checksum node['imos_devel']['talend']['codegen_source_checksum']
-  mode     0644
-  action   :create_if_missing
+node['imos_devel']['talend']['packages'].each do |pkg_desc|
+  install_talend_pkg(pkg_desc)
 end
 
 # Chown the talend directory to the desired user
