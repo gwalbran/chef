0cdeb32eaa146b64f5ab0d56063a71befaacaf47
Dan Fruehauf <malkodan@gmail.com>
2015-06-23 17:40:30 +1000

    imos_po: compact recipes, bulk install packages

--- cookbooks/imos_po/attributes/data_services.rb
+++ cookbooks/imos_po/attributes/data_services.rb
@@ -34,6 +34,8 @@ default['imos_po']['data_services']['lib']       = ::File.join(node['imos_po']['
 # When running in vagrant in dev mode, we will clone the repo from outside the node
 default['imos_po']['data_services']['clone_repository'] = true
 
+# Required packages
+default['imos_po']['data_services']['packages'] = [ 'imagemagick', 'gdal-bin', 'sqlite3', 'heirloom-mailx', 'python-pip', 'python-dev', 'libnetcdf-dev', 'libhdf5-serial-dev', 'python-scipy', 'python-matplotlib', 'python-numpy' ]
+
 # All required python plugins
-default['imos_po']['data_services']['packages'] = [ 'heirloom-mailx', 'python-pip', 'python-dev', 'libnetcdf-dev', 'libhdf5-serial-dev', 'python-scipy', 'python-matplotlib', 'python-numpy' ]
 default['imos_po']['data_services']['python']['plugins'] = [ 'netCDF4' ]
