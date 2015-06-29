80353a007eb2614fe1646fd202a285320dce9942
Dan Fruehauf <malkodan@gmail.com>
2015-06-29 10:53:38 +1000

    imos_devel: upgrade vagrant to 1.6.5

--- cookbooks/imos_devel/attributes/default.rb
+++ cookbooks/imos_devel/attributes/default.rb
@@ -1,7 +1,7 @@
-default['imos_devel']['vagrant']['version']            = '1.6.2'
-default['imos_devel']['vagrant']['package_name']       = 'vagrant_1.6.2_x86_64.deb'
+default['imos_devel']['vagrant']['version']            = '1.6.5'
+default['imos_devel']['vagrant']['package_name']       = 'vagrant_1.6.5_x86_64.deb'
 default['imos_devel']['vagrant']['source_url']         = "https://dl.bintray.com/mitchellh/vagrant/#{node['imos_devel']['vagrant']['package_name']}"
-default['imos_devel']['vagrant']['source_checksum']    = '1599113509b3ab2b13ca219c3bd29bd2fb91b7eb328629ef85036ef5f52677c4'
+default['imos_devel']['vagrant']['source_checksum']    = 'e2c7af6d032ac551ebd6399265cb9cb64402c9fb96a12289161b7f67afada28a'
 
 default['imos_devel']['virtualbox']['package_name']    = 'virtualbox-4.3_4.3.6-91406~Ubuntu~precise_amd64.deb'
 default['imos_devel']['virtualbox']['source_url']      = "http://download.virtualbox.org/virtualbox/4.3.6/#{node['imos_devel']['virtualbox']['package_name']}"
