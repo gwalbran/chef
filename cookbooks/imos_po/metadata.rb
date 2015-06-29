cb2e33bb5f3c7e635bae07e1bfa5d082b8c0b44f
Dan Fruehauf <malkodan@gmail.com>
2015-06-29 16:56:00 +1000

    imos_po: depend on python cookbook

--- cookbooks/imos_po/metadata.rb
+++ cookbooks/imos_po/metadata.rb
@@ -6,6 +6,6 @@ description      "Installs/Configures imos_po"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
-%w{ imos_mounts imos_core imos_users sudo logrotate rsyslog }. each do |cb|
+%w{ imos_mounts imos_core imos_users python sudo logrotate rsyslog }. each do |cb|
   depends cb
 end
