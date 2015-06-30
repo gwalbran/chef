c0b5730284b1fbbf3641a8c20786e465f1850dac
Dan Fruehauf <malkodan@gmail.com>
2015-06-30 19:49:27 +1000

    fix imos_chef_solo
    move to own recipe because of:
    https://github.com/edelight/chef-solo-search/issues/59

---
+++ cookbooks/imos_chef_solo/metadata.rb
@@ -0,0 +1,9 @@
+name             "imos_chef_solo"
+maintainer       "IMOS"
+maintainer_email "developers@emii.org.au"
+license          "All rights reserved"
+description      "chef-solo-search wrapper"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+depends 'chef-solo-search'
--- cookbooks/imos_core/metadata.rb
+++ cookbooks/imos_core/metadata.rb
@@ -11,7 +11,6 @@ version          "0.1.0"
   sysctl
   cron
   build-essential
-  chef-solo-search
   git_ssh_wrapper
   unattended_upgrades
   sudo
