5fba94feec29936a7d7cecd7e7a146fe50120414
Dan Fruehauf <malkodan@gmail.com>
2015-09-16 10:18:56 +1000

    imos_core: fix indent in cronjob.rb

--- cookbooks/imos_core/recipes/cronjob.rb
+++ cookbooks/imos_core/recipes/cronjob.rb
@@ -13,16 +13,16 @@ end
 
 node['cronjobs'].each do |job|
   cron_d job['job_name'] do
-    minute job['minute']    || "*"
-    hour job['hour']        || "*"
-    day job['day']          || "*"
-    month job['month']      || "*"
-    weekday job['weekday']  || "*"
+    minute  job['minute']  || "*"
+    hour    job['hour']    || "*"
+    day     job['day']     || "*"
+    month   job['month']   || "*"
+    weekday job['weekday'] || "*"
     command job['command']
-    user job['user']        || "root"
-    mailto job['mailto']    || node[:imos_core][:cronjob][:email_address]
-    path job['path']
-    home job['home']
-    shell job['shell']
+    user    job['user']    || "root"
+    mailto  job['mailto']  || node[:imos_core][:cronjob][:email_address]
+    path    job['path']
+    home    job['home']
+    shell   job['shell']
   end
 end if node['cronjobs']
