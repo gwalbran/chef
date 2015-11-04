67919a6ed3e1df17411558b22728a85030896035
Jon Burgess <jkburges@gmail.com>
2015-11-04 15:28:24 +1100

    Removed unused/empty files

--- cookbooks/imos_postgresql/metadata.rb
+++ cookbooks/imos_postgresql/metadata.rb
@@ -5,8 +5,6 @@ license          'All rights reserved'
 description      'Installs/Configures imos_postgresql'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
-recipe            "imos_postgresql::default", "Installs schema_support"
-recipe            "imos_postgresql::schema_support", "Installs postgresql database with support for schemas,extensions,permissions"
 
 %w{ backup }.each do |cb|
   depends cb
