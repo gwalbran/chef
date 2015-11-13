fc31f47459d8b53a16b0f26b6a7d3b821255d1c2
Dan Fruehauf <malkodan@gmail.com>
2015-11-13 11:59:16 +1100

    imos_vsftpd: Cleanups, use attributes

--- cookbooks/imos_vsftpd/attributes/default.rb
+++ cookbooks/imos_vsftpd/attributes/default.rb
@@ -5,3 +5,7 @@ default['imos_vsftpd']['ftp_users']['data_bags'] = []
 
 default['imos_vsftpd']['ftp_dir_tree']['data_bags'] = []
 default['imos_vsftpd']['ftp_dir_tree']['root'] = '/var/lib/ftp'
+
+default['imos_vsftpd']['ftp_dir_tree']['user']  = "ftp"
+default['imos_vsftpd']['ftp_dir_tree']['group'] = "users"
+default['imos_vsftpd']['ftp_dir_tree']['mode']  = 02775
--- cookbooks/imos_vsftpd/recipes/ftp_dir_tree.rb
+++ cookbooks/imos_vsftpd/recipes/ftp_dir_tree.rb
@@ -11,17 +11,17 @@ node['imos_vsftpd']['ftp_dir_tree']['data_bags'].each do |data_bag_pattern|
 
   # break something like 'ftp_dir_tree/*' to 'ftp_dir_tree' and '*'
   # or something like 'ftp_dir_tree/AATAMS to 'ftp_dir_tree' and 'AATAMS
-  data_bag_pattern_name     = File.dirname(data_bag_pattern)
-  data_bag_pattern_wildcard = File.basename(data_bag_pattern)
+  data_bag_pattern_name     = ::File.dirname(data_bag_pattern)
+  data_bag_pattern_wildcard = ::File.basename(data_bag_pattern)
 
   search("#{data_bag_pattern_name}", "id:#{data_bag_pattern_wildcard}").each do |data_bag|
     directory = File.join(root, data_bag['directory'])
     Chef::Log.info("Creating FTP directory '#{directory}'")
 
     directory directory do
-      owner     data_bag['owner']
-      group     data_bag['group']
-      mode      data_bag['mode']
+      owner     data_bag['user']  or node['imos_vsftpd']['ftp_dir_tree']['user']
+      group     data_bag['group'] or node['imos_vsftpd']['ftp_dir_tree']['group']
+      mode      data_bag['mode']  or node['imos_vsftpd']['ftp_dir_tree']['mode']
       recursive true
     end
 
