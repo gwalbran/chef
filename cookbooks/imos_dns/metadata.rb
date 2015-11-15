75aa984e14bb404907a2f9a8eebe11be2e66acb3
Dan Fruehauf <malkodan@gmail.com>
2015-11-16 10:25:44 +1100

    Remove ohai_plugins

--- cookbooks/imos_dns/metadata.rb
+++ cookbooks/imos_dns/metadata.rb
@@ -6,4 +6,3 @@ description      'Installs/Configures imos_dns'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends          'route53'
-depends          'ohai_plugins'
--- cookbooks/ohai_plugins/README.md
+++
@@ -1,4 +0,0 @@
-ohai_plugins Cookbook
-=====================
-Includes all of IMOS ohai plugins.
-
