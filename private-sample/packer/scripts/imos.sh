46921f62291dff18c7d4039cefd67226bc6f11f9
Dan Fruehauf <malkodan@gmail.com>
2015-12-07 16:12:59 +1100

    Packer box to install python modules
    Saves a lot of time when provisioning po box.
    
    Also fix packer template to not look for S3 data bags and such.

--- private-sample/packer/scripts/imos.sh
+++ private-sample/packer/scripts/imos.sh
@@ -1,3 +1,6 @@
+# Fix apt repositories to use main ones
+sed -i -e 's#http://us.archive.ubuntu.com/ubuntu/#http://archive.ubuntu.com/ubuntu/#g' /etc/apt/sources.list
+apt-get clean
 apt-get -y update
 
 # Install more packages
@@ -5,5 +8,13 @@ apt-get -y install curl nfs-common libxml2-dev libxslt1-dev zlib1g-dev \
     binutils-doc bison build-essential unzip gettext flex ncurses-dev lvm2 \
     auditd nagios-nrpe-server libnagios-plugin-perl nsca-client runit \
     vim
+
+apt-get -y install libxft-dev libpng-dev libblas-dev liblapack-dev gfortran \
+    python-dev python-pip python-psycopg2 libnetcdf-dev libhdf5-serial-dev
+
+pip install numpy==1.10.1
+pip install netCDF4==1.2.1
+pip install scipy==0.9.0
+pip install matplotlib==1.1.1
 
 exit
