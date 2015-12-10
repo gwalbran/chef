a4cf640aaa60e76cadb4f64ad958b5d899171164
Dan Fruehauf <malkodan@gmail.com>
2015-12-10 17:54:56 +1100

    imos_core: Improve XML injection function
     * Can inject XML to non-existing files
     * Build whole hierarchy when inserting new nodes
     * Unit tests

--- cookbooks/imos_core/libraries/xml_helper.rb
+++ cookbooks/imos_core/libraries/xml_helper.rb
@@ -19,39 +19,57 @@ class Chef::Recipe::XMLHelper
   # Simply loads an XML file with Nokogiri
   def self.load_xml_file(xml_file)
     require 'nokogiri'
-    Nokogiri::XML(File.open(xml_file))
+    begin
+      return Nokogiri::XML(File.open(xml_file))
+    rescue
+      return nil
+    end
   end
 
   # Returns value specified at given xpath, for example '/global/settings/proxyBaseUrl'
   def self.get_xml_value(xml_file, xpath)
     require 'nokogiri'
-    xpath_object = load_xml_file(xml_file).at_xpath(xpath)
+    xml_object = load_xml_file(xml_file) or return nil
+    xpath_object = xml_object.at_xpath(xpath)
     xpath_object ? xpath_object.inner_html : nil
   end
 
   # insert/updates xml node at 'xpath' with 'value'
   def self.insert_xml_node(xml_file, xpath, value)
     require 'nokogiri'
-    @xml = load_xml_file(xml_file)
-    xml_node = @xml.at_xpath(xpath)
-
-    if xml_node
-      # Node already exists, lets edit it
-      xml_node.inner_html = value.to_s
-    else
-      # Need to add a new node
-      # Split xpath from '/1/2/3' to '/1/2' and '3'
-      # We need to add node named '3' under '/1/2'
-      name = xpath.split("/").last
-      base_xpath = xpath.split('/')[0..-2].join('/') # remove last element
-      new_node = Nokogiri::XML::Node.new name, @xml
-      new_node.content = value.to_s
-      @xml.at_xpath(base_xpath).children.last.add_next_sibling(new_node)
+    xml = load_xml_file(xml_file)
+    if xml.nil?
+      xml = Nokogiri::XML("")
     end
+    xml_node = xml.at_xpath(xpath)
+
+    # If our xpath is /1/2/3, iterate on path and add nodes as we go and
+    # add/verify exists:
+    # /1
+    # /1/2
+    # /1/2/3
+    current_xpath = ""
+    xpath.split("/")[1..-1].each do |elem|
+      elem_xpath = current_xpath + "/" + elem
+
+      xml_node = xml.at_xpath(elem_xpath)
+      if ! xml_node
+        new_node = Nokogiri::XML::Node.new(elem, xml)
+        if current_xpath.empty?
+          xml.add_child(new_node)
+        else
+          xml.at_xpath(current_xpath).add_child(new_node)
+        end
+      end
+      current_xpath = elem_xpath
+    end
+
+    # Finally set the node text to the value passed to the function
+    xml.at_xpath(xpath).inner_html = value.to_s
 
     # Write out the xml again
     File.open(xml_file, "w") do |data|
-      data << @xml
+      data << xml
     end
   end
 
