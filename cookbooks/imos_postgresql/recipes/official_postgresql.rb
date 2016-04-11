dc43a478137583c74bdfe75991846d318f29ae6f
bhasin85 <bhasin85@gmail.com>
2016-04-11 14:21:49 +1000

    Updated keyserver url

--- cookbooks/imos_postgresql/recipes/official_postgresql.rb
+++ cookbooks/imos_postgresql/recipes/official_postgresql.rb
@@ -22,7 +22,7 @@ apt_repository 'official_postgres' do
   uri 'http://apt.postgresql.org/pub/repos/apt/'
   distribution "#{node['lsb']['codename']}-pgdg"
   components ['main']
-  keyserver 'keyserver.ubuntu.com'
+  keyserver 'hkp://keyserver.ubuntu.com:80'
   key 'ACCC4CF8'
 end
 
