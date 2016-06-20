8ed173c654a95d0892ca554f46c7a5b4e1009dfd
Leigh Gordon <leigh.gordon@utas.edu.au>
2016-06-20 12:12:59 +1000

    Update PROJ dependency to libproj-dev due to missing symlink being part of dev package

--- cookbooks/imos_po/attributes/data_services.rb
+++ cookbooks/imos_po/attributes/data_services.rb
@@ -57,7 +57,7 @@ default['imos_po']['data_services']['packages'] = [
   'lftp',
   'libhdf5-serial-dev',
   'libnetcdf-dev',
-  'libproj0', # For Python/GDAL
+  'libproj-dev', # For Python/GDAL
   'mdbtools',
   'p7zip',
   'python-beautifulsoup',
