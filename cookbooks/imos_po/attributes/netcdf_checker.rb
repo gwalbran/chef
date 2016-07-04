7736a03db19196f0b94a1c8a246cf99c3b631768
anguss00 <Angus.Scheibner@utas.edu.au>
2016-07-04 14:33:35 +1000

    Deploy compliance_checker python eggs built using jenkins

--- cookbooks/imos_po/attributes/netcdf_checker.rb
+++ cookbooks/imos_po/attributes/netcdf_checker.rb
@@ -1,5 +1,8 @@
-default['imos_po']['netcdf_checker']['dir']           = "/var/lib/netcdf-checker"
+default['imos_po']['netcdf_checker']['dir']           = "/usr/local/bin"
 default['imos_po']['netcdf_checker']['repo']          = "https://github.com/ioos/compliance-checker.git"
 default['imos_po']['netcdf_checker']['branch']        = "master"
 default['imos_po']['netcdf_checker']['executable']    = "/usr/local/bin/netcdf-checker"
-default['imos_po']['netcdf_checker']['cc_plugin_dir'] = ::File.join(node['imos_po']['data_services']['dir'], "lib", "cc_plugin_imos")
+default['imos_po']['netcdf_checker']['version'] = '2.2.0'
+default['imos_po']['netcdf_checker']['url'] = "https://ci.aodn.org.au/job/compliance_checker/lastSuccessfulBuild/artifact/dist/compliance_checker-#{default['imos_po']['netcdf_checker']['version']}-py2.7.egg"
+default['imos_po']['netcdf_checker']['cc_plugin_ver'] = '0.9.0'
+default['imos_po']['netcdf_checker']['cc_plugin_url'] = "https://ci.aodn.org.au/job/compliance_checker_plugin_imos/lastSuccessfulBuild/artifact/lib/cc_plugin_imos/dist/cc_plugin_imos-#{default['imos_po']['netcdf_checker']['cc_plugin_ver']}-py2.7.egg"
