fd3a66c5690ce7277df0fb5588daa022834a3cdf
Leigh Gordon <leigh.gordon@utas.edu.au>
2016-07-04 18:12:34 +1000

    wip: add s3_plugin_profile recipe

---
+++ cookbooks/imos_jenkins/recipes/s3_plugin_profile.rb
@@ -0,0 +1,37 @@
+#
+# Cookbook Name:: jenkins
+# Recipe:: s3_plugin_profile
+#
+# Copyright 2016, IMOS
+#
+# All rights reserved - Do Not Redistribute
+#
+# Recipe to define all slaves on a master
+#
+
+jenkins_user_data_bag = Chef::EncryptedDataBagItem.load("users", node['imos_jenkins']['user'])
+
+jenkins_script 'create S3 profile' do
+  command <<-EOH.gsub(/^ {4}/, '')
+    import jenkins.model.*
+    import hudson.plugins.s3.*
+
+    def instance = Jenkins.getInstance()
+    def desc = inst.getDescriptor("hudson.plugins.s3.S3BucketPublisher")
+
+    def s3profile = new S3Profile(
+      "S3Profile-jenkins",
+      "#{jenkins_user_data_bag['access_key_id']}",
+      "#{jenkins_user_data_bag['secret_access_key']}",
+      false,
+      5,
+      "5",
+      "5",
+      "5",
+      "5",
+      false
+    )
+
+    instance.save()
+  EOH
+end
