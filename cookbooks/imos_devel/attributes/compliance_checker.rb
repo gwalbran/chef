54537da0344b1dae4dd40762ef4c994c80c7b164
Leigh Gordon <leigh.gordon@utas.edu.au>
2016-07-04 14:31:54 +1000

    Add imos_devel::compliance_checker recipe, and add to imos_jenkins

---
+++ cookbooks/imos_devel/attributes/compliance_checker.rb
@@ -0,0 +1,1 @@
+default['imos_devel']['compliance_checker']['python_requirements_url'] = 'https://raw.githubusercontent.com/aodn/compliance-checker/master/requirements.txt'
---
+++ cookbooks/imos_devel/recipes/compliance_checker.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: imos_devel
+# Recipe:: compliance_checker
+#
+# Copyright 2013, IMOS
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# Install Compliance Checker prerequisites
+requirements_filename  = File.basename(node['imos_devel']['compliance_checker']['python_requirements_url'])
+requirements_full_path = File.join(Chef::Config[:file_cache_path], requirements_filename)
+
+remote_file requirements_full_path do
+  source   node['imos_devel']['compliance_checker']['python_requirements_url']
+  mode     0644
+  action   :create_if_missing
+end
+
+pip_requirements requirements_full_path
