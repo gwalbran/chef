8d214093adbd182d7b84f83770ddd487ac00a875
Angus Scheibner <angus.scheibner@utas.edu.au>
2017-01-12 15:47:18 +1100

    Revert "Fix backup cookbook for newer Chef versions"

--- cookbooks/backup-cookbook/recipes/default.rb
+++ cookbooks/backup-cookbook/recipes/default.rb
@@ -24,11 +24,14 @@ node[:backup][:packages].each do |package|
   package package
 end
 
-# Create backup user/group
-group node[:backup][:group]
-user node[:backup][:username] do
-  home node[:backup][:base_dir]
-  gid  node[:backup][:group]
+# Create backup user but only if it doesn't exist
+if ! User(node[:backup][:username])
+  group node[:backup][:group]
+
+  user node[:backup][:username] do
+    home node[:backup][:base_dir]
+    gid  node[:backup][:group]
+  end
 end
 
 # Honour the fact that sometimes the backup directory may be on a NFS volume or
