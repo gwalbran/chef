483c54a8af0651010f8356762b9adf6def4b71ff
Leigh Gordon <leigh.gordon@utas.edu.au>
2017-01-20 15:06:39 +1100

    Update postfix attributes for newer cookbook version

--- cookbooks/imos_postfix/attributes/default.rb
+++
@@ -1,1 +0,0 @@
-default['postfix']['sasl_password_file'] = "#{node['postfix']['conf_dir']}/sasl_passwd"
--- cookbooks/imos_postfix/recipes/default.rb
+++ cookbooks/imos_postfix/recipes/default.rb
@@ -2,22 +2,21 @@
 # Cookbook Name:: imos_postfix
 # Recipe:: default
 #
-# Copyright 2016, YOUR_COMPANY_NAME
+# Copyright 2016, IMOS
 #
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "postfix::sasl_auth"
-
-node.override['postfix']['smtp_sasl_auth_enable']= 'yes'
-node.override['postfix']['smtp_sasl_security_options']= 'noanonymous'
-node.override['postfix']['smtp_sasl_password_maps']= 'hash:/etc/postfix/sasl_passwd'
-node.override['postfix']['smtp_tls_cafile']= "/opt/chef/embedded/ssl/certs/cacert.pem"
-node.override['postfix']['smtp_tls_security_level']= 'encrypt'
+node.override['postfix']['main']['smtp_sasl_auth_enable']= 'yes'
+node.override['postfix']['main']['smtp_sasl_security_options']= 'noanonymous'
+node.override['postfix']['main']['smtp_sasl_password_maps']= 'hash:/etc/postfix/sasl_passwd'
+node.override['postfix']['main']['smtp_tls_CAfile']= "/opt/chef/embedded/ssl/certs/cacert.pem"
+node.override['postfix']['main']['smtp_use_tls'] = 'yes'
+node.override['postfix']['sasl_password_file'] = "#{node['postfix']['conf_dir']}/sasl_passwd"
 
 sendgrid_vars = Chef::DataBagItem.load('passwords','sendgrid')
-node.override['postfix']['relayhost']= "[" + sendgrid_vars['host'] + "]:" + sendgrid_vars['port'].to_s
-node.override['postfix']['smtp_sasl_passwd']= sendgrid_vars['password']
-node.override['postfix']['smtp_sasl_user_name']= sendgrid_vars['username']
+node.override['postfix']['main']['relayhost']= "[" + sendgrid_vars['host'] + "]:" + sendgrid_vars['port'].to_s
+node.override['postfix']['sasl']['smtp_sasl_passwd']= sendgrid_vars['password']
+node.override['postfix']['sasl']['smtp_sasl_user_name']= sendgrid_vars['username']
 
-
+include_recipe "postfix::sasl_auth"
