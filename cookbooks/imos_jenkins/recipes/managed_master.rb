ee162256b187819fe4ee73bf8c3a41e13485112b
anguss00 <Angus.Scheibner@utas.edu.au>
2017-05-01 16:00:40 +1000

    Mock jenkins ssh in dev env, use readonly deploy keys in prod

--- cookbooks/imos_jenkins/recipes/managed_master.rb
+++ cookbooks/imos_jenkins/recipes/managed_master.rb
@@ -11,6 +11,10 @@
 include_recipe 'imos_java'
 include_recipe 'imos_jenkins::node_common'
 
+jenkins_home = node['imos_jenkins']['master']['home']
+scm_repo = node['imos_jenkins']['scm_repo']
+ssh_wrapper = File.join("#{jenkins_home}", '.ssh', 'wrappers', 'git_deploy_wrapper.sh')
+
 node.set['jenkins']['master']['runit']['sv_timeout'] = 240
 node.set['jenkins']['master']['jvm_options'] = node['imos_jenkins']['master']['jvm_options']
 
@@ -26,28 +30,16 @@ sudo 'jenkins' do
   nopasswd true
 end
 
-jenkins_home = node['imos_jenkins']['master']['home']
-scm_repo = node['imos_jenkins']['scm_repo']
-
-ssh_wrapper = File.join("#{jenkins_home}", '.ssh', 'wrappers', 'git_deploy_wrapper.sh')
-
-# Overwrite local files with those present in SCM
-execute 'init_jenkins_scm' do
-  command "git rev-parse --is-inside-work-tree || { git init && git remote add origin #{scm_repo} && git fetch && git reset --hard origin/master; }"
-  cwd jenkins_home
-  user node['imos_jenkins']['user']
-  group node['imos_jenkins']['group']
-end
-
 require 'openssl'
 require 'net/ssh'
 
-jenkins_ssh_key = Chef::EncryptedDataBagItem.load("users", "chef")['ssh_private_key']
-key = OpenSSL::PKey::RSA.new(jenkins_ssh_key)
-private_key = key.to_pem
+if !Chef::Config['dev']
+  jenkins_ssh_key = Chef::EncryptedDataBagItem.load("users", "chef")['ssh_private_key']
+  key = OpenSSL::PKey::RSA.new(jenkins_ssh_key)
+  private_key = key.to_pem
 
-public_key = "#{key.ssh_type} #{[key.to_blob].pack('m0')}"
-node.run_state[:jenkins_private_key] = private_key
+  node.run_state[:jenkins_private_key] = private_key
+end
 
 node['imos_jenkins']['plugins'].each do |plugin_id, version|
   jenkins_plugin plugin_id do
@@ -56,14 +48,14 @@ node['imos_jenkins']['plugins'].each do |plugin_id, version|
   end
 end
 
-execute 'git_ssh' do
-  command "GIT_SSH=#{ssh_wrapper}"
-  user node['imos_jenkins']['user']
-  group node['imos_jenkins']['group']
-  notifies :run, "execute[init_jenkins_scm]", :immediately
-end
-
-
+# Overwrite local files with those present in SCM
+ execute 'init_jenkins_scm' do
+   command "git rev-parse --is-inside-work-tree || { git init && git remote add origin #{scm_repo} && git fetch && git reset --hard origin/master; }"
+   cwd jenkins_home
+   user node['imos_jenkins']['user']
+   group node['imos_jenkins']['group']
+   environment ({"GIT_SSH" => "#{ssh_wrapper}"})
+ end
 
 # AWS passwords
 envvars = {}
