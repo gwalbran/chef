b0521e007f21bf13d160831475d1477be59e4891
Leigh Gordon <leigh.gordon@utas.edu.au>
2017-06-29 11:09:28 +1000

    Update vsftpd pam template for Ubuntu 16.04

--- cookbooks/imos_vsftpd/recipes/pam.rb
+++ cookbooks/imos_vsftpd/recipes/pam.rb
@@ -9,10 +9,15 @@
 
 package "libpam-pwdfile"
 
+pam_pwdfile_lib = node['lsb']['codename'].include?("precise") ? '/lib/security/pam_pwdfile.so' : '/lib/x86_64-linux-gnu/security/pam_pwdfile.so'
+
 # PAM for vsftpd authentication
 template "/etc/pam.d/pam_vsftpd" do
   source "pam_vsftpd.erb"
   owner  "root"
   group  "root"
   mode   00644
+  variables(
+    :pam_pwdfile_lib => pam_pwdfile_lib
+  )
 end
